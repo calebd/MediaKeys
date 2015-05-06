bcc862df313ac3ae41d160ea442ebffd4e1ccdeb
Caleb Davenport <caleb@calebd.me>
2015-05-06 01:08:22 -0700

    Add Xcode empty project.

---
+++ MediaKeys/MediaKeys.h
@@ -0,0 +1,19 @@
+//
+//  MediaKeys.h
+//  MediaKeys
+//
+//  Created by Caleb Davenport on 5/6/15.
+//  Copyright (c) 2015 Caleb Davenport. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MediaKeys.
+FOUNDATION_EXPORT double MediaKeysVersionNumber;
+
+//! Project version string for MediaKeys.
+FOUNDATION_EXPORT const unsigned char MediaKeysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaKeys/PublicHeader.h>
+
+
